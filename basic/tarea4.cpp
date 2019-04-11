949bac436ff183ee961677307e7839f14c196165
danielaso5401 <42683702+danielaso5401@users.noreply.github.com>
2019-04-11 15:29:49 -0500

    Add files via upload

---
+++ basic/tarea4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int cont=91;
+  char listo;
+  for( int a=65;cont!=65;++a and --cont){
+    listo=a;
+    cout<<listo<<'\t'<<a<<'\n';
+
+  }
+  for(int r=0; r!=10; r++){
+    cout<<r<<'\n';
+  }
+}
---
+++ basic/tarea5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int square(int x){
+	int r=0;
+	for(int i=0;i<x;i++){
+		r=r+x;	
+		
+	}
+	return r;
+}
+
+int main(){
+	int l;
+	cin>>l
+	cout<<square(l);
+}
