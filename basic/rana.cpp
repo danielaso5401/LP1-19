81448473487000cb6611292570de7f27c1b296e1
danielaso5401 <42683702+danielaso5401@users.noreply.github.com>
2019-04-26 16:44:22 -0400

    Update rana.cpp

--- basic/rana.cpp
+++ basic/rana.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 using namespace std;
 int main(){
+	int contador=0;
 	int km;
 	cin>>km;
 	int saltos=0;
 	while(km>0){
-		km=km-9;
+		km=km-contador;
 		++saltos;
+		contador=contador+9;
 	}
 	cout<<saltos;
 	return 0;
